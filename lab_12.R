18735e551731c594807494d9c0821ecf58a40669
Sana Miyauchi <sanamiyauchi@Sanas-Air.wv.cc.cmu.edu>
2018-04-18 14:00:46 -0400

    2b model_select

--- lab_12.R
+++ lab_12.R
@@ -1,6 +1,13 @@
 generate_data = function(n,p){
   covariates = matrix(rnorm(n*p,0,1),nrow = n, ncol = p)
   responses = rnorm(n, 0,1)
-  return (list(covariates,responses))
+  return (list(covariates=covariates,responses = responses))
+  }
+
+model_select = function(covariates, responses, cutoff){ 
+  data = list(covariates = covariates, responses = responses)
+  cov.lm = lm(responses ~ covariates, data)
+  index.cutoff = which(summary(cov.lm)$coefficients[,4]  <= cutoff)
+  return(lm(responses~covariates[,index.cutoff], data))
   }
 
